796ab710bca83bf860b8de8170210a4f9623c82d
Adedipupo <jokanolaadedipupo@gmail.com>
2026-01-25 05:40:28 +0100

    auth page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import {
   Outlet,
 } from "react-router-dom";
 
-import Quiz from "./components/cube/Quiz/Quiz.jsx";
+// import Quiz from "./components/cube/Quiz/Quiz.jsx";
 import QuizApi from "./components/cube/Quiz/QuizApi.jsx";
 
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Quiz />} />
-      <Route path="/quiz2" element={<QuizApi />} />
+      {/* Default route uses API-backed quiz with auth and backend state */}
+      <Route path="/" element={<QuizApi />} />
+      {/* Optional local/static quiz without backend, if you still want it */}
+      {/* <Route path="/quiz-local" element={<Quiz />} /> */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
